de0650caf2634725bb50d403b01d06f186505096
AnjanaKurdina <85196006+AnjanaKurdina@users.noreply.github.com>
2021-10-13 12:36:38 -0400

    added search engine

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
         <header className="App-header">Dictionary App</header>
-
+        <main>
+          <Dictionary />
+        </main>
         <footer>
           <div className="Footer">
             This website was coded by{" "}
